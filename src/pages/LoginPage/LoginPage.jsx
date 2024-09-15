7e35097e20e8770df45718e76ea056ddb6f3eb07
Florencia <flordani98@gmail.com>
2024-09-14 22:19:14 -0300

    Modifica estilos de loginPage
    Añade logo de app y variables de css

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,30 +2,41 @@
 import React from 'react';
 import styles from './LoginPage.module.css';
 import PropTypes from 'prop-types';
+import fondoTrabajo from '../../assets/fondo-trabajo.jpg'
+import logoApp from '../../assets/logo-app.png'
 
 const LoginPage = ({ }) => {
 	return (
-		<div className={styles.loginpage}>
+		<div className={styles['login-container']}>
 			{/* <div>
 				<button>Registrarse</button>
 				<button>Iniciar sesión</button>
 			</div> */}
 
-			<form>
-				<div className={styles['form-group']}>
-					<label htmlFor='email'>Email</label>
-					<input type='email' id='email' name='email' required />
+			<div>
+				<img className={styles['login-image']} src={fondoTrabajo} alt="elementos de organización en el trabajo"></img>
+			</div>
 
-				</div>
+			<div className={styles['form-content']}>
 
-				<div className={styles['form-group']}>
-					<label htmlFor='password'>Contraseña</label>
-					<input type='password' id='password' name='password' required />
-				</div>
+				
+				<img className={styles['app-logo']} src={logoApp} alt="logo de empresa"></img>
+				<h3 className={styles['form-title']}>Inicio de sesión</h3>
+				<form>
+					<div className={styles['form-group']}>
+						<label htmlFor='email'>Email</label>
+						<input type='email' id='email' name='email' placeholder='Ingrese su email' required />
 
-				<button className={styles['btn-submit']} type='submit'>Iniciar sesión</button>
-			</form>
+					</div>
 
+					<div className={styles['form-group']}>
+						<label htmlFor='password'>Contraseña</label>
+						<input type='password' id='password' name='password' placeholder='Ingrese su contraseña' required />
+					</div>
+
+					<button className={styles['btn-submit']} type='submit'>Iniciar sesión</button>
+				</form>
+			</div>
 		</div>
 	);
 };
